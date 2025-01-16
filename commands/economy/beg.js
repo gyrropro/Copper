afb9dc911a9e802a66fcf16305ca8532aa9688b2
gyrropro <gabrielbor.1324@gmail.com>
2025-01-16 10:28:10 -0500

    init

---
+++ commands/economy/beg.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, Client } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('beg')
+		.setDescription('Beg for coins'),
+	async execute(interaction) {
+        const embed = new EmbedBuilder()
+	    .setColor(0xc56a39)
+	    .setTitle('Coming soon!')
+		.addFields({ value: `<t:${Math.floor(interaction.client.readyTimestamp / 1000)}:R>`, name: 'online since:' })
+        .setTimestamp()
+        .setFooter({ text: 'Copper', iconURL: 'https://i.imgur.com/ySErtc7.png' });
+
+		await interaction.reply({ embeds: [embed] });
+	},
+};
